4c179d9d430149063517de108c902d120c7ad6ec
Danny Shen <shenfive@gmail.com>
2020-08-08 11:51:44 +0800

    Init TableView

--- a207HelloTableView/ViewController.swift
+++ a207HelloTableView/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
 
+
+    @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myTableView.dataSource = self
+        
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
 
 }
 
